9664a4afdaf43aea78dda8ddac232784cb5a0914
kian_ja <kian85ja@gmail.com>
2025-07-23 05:07:04 +0330

    add shodan class delete method

--- dormitory/obj/Debug/net8.0/dormitory.AssemblyInfo.cs
+++ dormitory/obj/Debug/net8.0/dormitory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dormitory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51a85a125ee8bed76661c72c5de621d8d961e3e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78ed1b89dc9f197e529aa9935737ad2905f1cd1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("dormitory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dormitory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
