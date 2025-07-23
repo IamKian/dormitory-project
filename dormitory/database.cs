51a85a125ee8bed76661c72c5de621d8d961e3e5
kian_ja <kian85ja@gmail.com>
2025-07-23 03:58:58 +0330

    tanzim ravabet va kelid haye khareji

--- dormitory/database.cs
+++ dormitory/database.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace dormitory
+public class DormitoryContext : DbContext
 {
-    class database
+    public DbSet<Dormitory> Dormitories { get; set; }
+    public DbSet<Block> Blocks { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<Tool> Tools { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Supervisor> Supervisors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
+        optionsBuilder.UseSqlite("Data Source=Dormitory.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Dormitory>()
+            .HasMany(d => d.Blocks)
+            .WithOne()
+            .HasForeignKey(b => b.DormitoryId);
+        modelBuilder.Entity<Block>()
+            .HasMany(b => b.Rooms)
+            .WithOne()
+            .HasForeignKey(r => r.BlockId);
+        modelBuilder.Entity<Room>()
+            .HasMany(r => r.Tools)
+            .WithOne()
+            .HasForeignKey(t => t.RoomId);
+        modelBuilder.Entity<Room>()
+            .HasMany(r => r.Students)
+            .WithOne()
+            .HasForeignKey(s => s.RoomId);
+        modelBuilder.Entity<Dormitory>()
+            .HasMany(d => d.Students)
+            .WithOne()
+            .HasForeignKey(s => s.DormitoryId);
+        modelBuilder.Entity<Dormitory>()
+            .HasMany(d => d.Supervisors)
+            .WithOne()
+            .HasForeignKey(s => s.DormitoryId);
     }
 }
